51f3ffec6dbc6e2003e2df168c8a359bd47378c2
Ravish_Satwani <168067570+Ravish-Satwani@users.noreply.github.com>
2024-04-25 14:02:42 +0530

    Solved program1.js

--- program1.js
+++ program1.js
@@ -1,5 +1,19 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+    let maxLength = 0;
+    let start = 0;
+    let arr = {};
+
+    for (let i = 0; i < s.length; i++){
+        const currChar = s[i];
+        
+        if (arr[currChar] !== undefined && arr[currChar] >= start) {
+            start = arr[currChar] + 1; 
+        }
+        arr[currChar] = i; 
+
+        maxLength = Math.max(maxLength, i - start + 1);
+    }
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
