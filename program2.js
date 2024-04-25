058bdef15063d83cc501f2095b64d68016f9bd1c
Ravish_Satwani <168067570+Ravish-Satwani@users.noreply.github.com>
2024-04-25 14:03:54 +0530

    Solved program2.js

--- program2.js
+++ program2.js
@@ -1,6 +1,33 @@
-function smallestMissingPositiveInteger(nums) {
+function smallestMissingPositiveInteger(A) {
 
-  // Implement the function smallest_missing_positive_integer
+  let n = A.length;
+        
+        let curr = new Array(n+1);
+        
+        // intializing it with 0 (false)
+        for(let i=0;i<n+1;i++)
+        {
+            curr[i]=false;
+        }
+        
+        // travesing the input and marking it with 1 (true)
+        for (let i = 0; i < n; i++)
+        {
+            if (A[i] > 0 && A[i] <= n)
+            {
+                curr[A[i]] = true;
+            }
+        }
+
+        // traversing again and looking for int wih value false
+        for (let i = 1; i <= n; i++)
+        {
+            if (!curr[i])
+            {
+                return i;
+            }
+        }
+        return n + 1;
 
 }
 
